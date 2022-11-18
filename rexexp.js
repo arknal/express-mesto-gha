cf9ca4fc327046d349b5d08b83d5c5b81c815d9b
Andrey Mikulin <mikulin1999@gmail.com>
2022-11-19 00:44:18 +0500

    удален лишний файл

--- rexexp.js
+++
@@ -1,3 +0,0 @@
-const regExp = /^http?:\/\/(?:www\.?)/;
-const reg = /^http?:\/\/(?:www\.)?[a-zA-Z0-9а-яА-я-]{1,}\.[a-zA-Zа-яА-я]{1,}\b(?:[a-zA-Z0-9._~:/?#[]@!\$&'()\*+,;=-])*/;
-console.log('http://ya./path/to/deep/$$$$$$'.search(reg));
