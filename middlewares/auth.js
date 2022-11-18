769eca337b4de52778a578381b5726249f3bf817
Andrey Mikulin <mikulin1999@gmail.com>
2022-11-18 23:36:07 +0500

    исправлено дефолтное значение переменной окружения в authMiddleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,12 @@ const jwt = require('jsonwebtoken');
 const ApiError = require('../error/apiError');
 const { unauthorizedStatusCode } = require('../utils/consts');
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET = 'e5941b231be3be054dcec54b7cf2f9f7' } = process.env;
 
 function authMiddleware(req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    console.log('Token not found');
     throw new ApiError(unauthorizedStatusCode, 'Необходима авторизация');
   }
 
