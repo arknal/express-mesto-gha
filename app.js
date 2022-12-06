d7ead55632bb0d6e61a7afdd092ea2f302b7700a
Andrey Mikulin <mikulin1999@gmail.com>
2022-12-07 02:51:30 +0500

    реализован логгер

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 require('dotenv').config();
-
 const express = require('express');
-
 const mongoose = require('mongoose');
-
 const { errors } = require('celebrate');
-
 const cors = require('cors');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
-
 const errorHandler = require('./middlewares/errorHandler');
+const NotFoundError = require('./error/NotFoundError');
 
 const app = express();
 
@@ -20,8 +16,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(requestLogger);
 app.use('/', router);
-router.use(errors());
+app.use(errorLogger);
+app.use(errors());
 app.use(errorHandler);
-
+app.use(() => {
+  throw new NotFoundError('404 Not Found');
+});
 app.listen(PORT, () => console.log('server started at port', PORT));
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,6 @@ const cardsRoutes = require('./cards');
 
 const { urlRegExp } = require('../utils/consts');
 
-const NotFoundError = require('../error/NotFoundError');
-
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -31,9 +29,5 @@ router.post('/signup', celebrate({
 router.use('/', userRoutes);
 
 router.use('/', cardsRoutes);
-
-router.use(() => {
-  throw new NotFoundError('404 Not Found');
-});
 
 module.exports = router;
