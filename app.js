5f52e012ca31c625a19798e193b6c7c4a0992b7c
Andrey Mikulin <mikulin1999@gmail.com>
2022-12-07 01:16:50 +0500

    cors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 
 const { errors } = require('celebrate');
 
+const cors = require('cors');
+
 const router = require('./routes');
 
 const errorHandler = require('./middlewares/errorHandler');
@@ -17,9 +19,9 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
-
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use('/', router);
 router.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT);
+app.listen(PORT, () => console.log('server started at port', PORT));
